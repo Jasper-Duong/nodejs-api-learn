6939c959385d45ec2e99d230ffa2045ec02f071c
Jasper-Duong <duongngochung13@gmail.com>
2023-02-08 00:08:57 +0700

    POST method in express

--- express/src/index.js
+++ express/src/index.js
@@ -4,7 +4,9 @@ const { timer } = require("./middlewares");
 const app = express();
 const PORT = 8080;
 
+// MIDDLEWARES
 app.use(timer);
+app.use(express.json());
 
 app.get("/friends", (req, res) => {
   res.send(friends);
@@ -15,6 +17,22 @@ app.get("/friends/:id", (req, res) => {
   const id = +req.params.id;
   res.json(friends.filter((ele) => ele.id === id)[0]);
   console.log(`Get friend with ID ${id} Successfully`);
+});
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name || !req.body.age) {
+    return res.status(400).json({
+      error: "Friend is in invalid shape!",
+    });
+  }
+  const { name, age } = req.body;
+  const newFriend = {
+    id: friends.length, 
+    name,
+    age,
+  };
+  friends.push(newFriend);
+  res.json(newFriend);
 });
 
 app.listen(PORT, () => {
