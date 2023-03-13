c6fc7a9fe4c5e03e55c4e1495fb6ba75d6c9d3e2
Jasper-Duong <duongngochung13@gmail.com>
2023-03-13 22:52:21 +0700

    Friends model

---
+++ express/src/models/friends.models.js
@@ -0,0 +1,24 @@
+const friends = [
+  {
+    id: 0,
+    name: "Andy",
+    age: 20,
+  },
+  {
+    id: 1,
+    name: "Billy",
+    age: 21,
+  },
+  {
+    id: 2,
+    name: "Cindy",
+    age: 22,
+  },
+  {
+    id: 3,
+    name: "Dany",
+    age: 23,
+  },
+];
+
+module.exports = { friends };
